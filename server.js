e5dda070ed06ffee49bd511eb3af7aa56fc70bcc
ItLena <ebordinghaus@gmail.com>
2024-06-17 14:00:45 +0200

    styling

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-const express = require ("express");
+const bodyParser = require("body-parser");
+const express = require("express");
+const fs = require("fs"); 
 
 const app = new express();
 const portNr = 8080;
+const filePath = "./data.json"
 
+// Start server
 app.listen(portNr, () =>{
     console.log (`Port ${portNr} is on`)
 })
 
+// Payload tools
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Endpoints 
+app.get("", (req,res)=>{
+    console.log("Hello world")
+})
